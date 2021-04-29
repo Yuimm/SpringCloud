36ea8af0b9133c2ee5f7d8f2f315da1a202c078a
yuimm <974288521@qq.com>
2021-04-30 01:29:13 +0800

    EurekaServer服务端安装

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SpringCloud
 SpringCloud学习
 内含SpringCloud Hoxton和SpringCloud alibaba
+
+笔记地址：https://www.hechuanteng.com/posts/springcloud.html 或 
+        https://yuimm.gitee.io/posts/springcloud.html
+
+视频地址：https://www.bilibili.com/video/BV18E411x7eT
---
+++ cloud-eureka-server7001/src/main/java/com/hct/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.hct.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description:
+ * @author: HCT
+ * @create: 2021/04/30 01:26
+ * @VVVersion 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
