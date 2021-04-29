5e4b57b55ded0ffd02645d11aef5dc5fd1346da8
yuimm <974288521@qq.com>
2021-04-30 01:03:03 +0800

    新建commons进行工程重构

--- cloud-provider-payment8001/src/main/java/com/hct/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hct/springcloud/controller/PaymentController.java
@@ -3,7 +3,6 @@ package com.hct.springcloud.controller;
 
 import com.hct.springcloud.pojos.CommonResult;
 import com.hct.springcloud.pojos.Payment;
-import com.hct.springcloud.pojos.R;
 import com.hct.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -23,29 +22,30 @@ public class PaymentController
     @Resource
     private PaymentService paymentService;
 
-    //只传给前端CommonResult，不需要前端了解其他的组件
     @PostMapping(value = "/payment/create")
-    public R create(@RequestBody Payment payment){
+    public CommonResult create(@RequestBody Payment payment)
+    {
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
-        if(result > 0){
-            return R.ok();
+
+        if(result > 0)
+        {
+            return new CommonResult(200,"插入数据库成功",result);
         }else{
-            return R.error();
+            return new CommonResult(444,"插入数据库失败",null);
         }
     }
+
     @GetMapping(value = "/payment/get/{id}")
-    public R getPaymentById(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
         Payment payment = paymentService.getPaymentById(id);
-        log.info("*****查询结果："+payment);
+
         if(payment != null)
         {
-            //return new CommonResult(200,"查询成功,serverPort:  ",payment);
-            return R.ok().data("payment",payment);
+            return new CommonResult(200,"查询成功",payment);
         }else{
-           // return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
-            return R.error();
+            return new CommonResult(201,"没有对应记录,查询ID: "+id,null);
         }
-
     }
 }
