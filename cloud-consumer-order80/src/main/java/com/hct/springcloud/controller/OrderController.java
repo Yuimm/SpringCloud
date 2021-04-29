5f114d64e6ae67ab3b386b1247d7de0ecd118cc1
yuimm <974288521@qq.com>
2021-04-30 02:32:07 +0800

    支付微服务集群配置--负载均衡

--- cloud-consumer-order80/src/main/java/com/hct/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/hct/springcloud/controller/OrderController.java
@@ -20,7 +20,9 @@ import javax.annotation.Resource;
 @RestController
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    //public static final String PAYMENT_URL = "http://localhost:8001";
+
+    public static final String PAYMENT_URL = "http://CLOUD-PROVIDER-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
