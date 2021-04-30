df4f339a1abe986328f76b984241f206e6afb6e9
yuimm <974288521@qq.com>
2021-04-30 18:43:35 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/hct/springcloud/OrderFeignMain80.java
@@ -0,0 +1,19 @@
+package com.hct.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @description:
+ * @author: HCT
+ * @create: 2021/04/30 18:33
+ * @VVVersion 1.0
+ **/
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/hct/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,18 @@
+package com.hct.springcloud.service;
+
+
+import com.hct.springcloud.pojos.CommonResult;
+import com.hct.springcloud.pojos.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-SERVICE")
+public interface PaymentFeignService
+{
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
