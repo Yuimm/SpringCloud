931ac607dffee2e7ddd488c76d24516f223b28a9
yuimm <974288521@qq.com>
2021-05-01 19:06:11 +0800

    Nacos服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/hct/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,22 @@
+package com.hct.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: HCT
+ * @create: 2021/05/01 16:52
+ * @VVVersion 1.0
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/hct/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.hct.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: HCT
+ * @create: 2021/05/01 16:52
+ * @VVVersion 1.0
+ **/
+@RestController
+@RefreshScope //支持Nacos的动态刷新功能。
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
